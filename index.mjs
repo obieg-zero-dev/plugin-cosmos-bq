f14b63cf2636f7ae28046cdd0933030e3a588284
Karamba Bober <gotoreadyai@gmail.com>
2026-05-06 22:11:07 +0200

    Rebuild: CastShadow optimizeSpeed

--- index.mjs
+++ index.mjs
@@ -3450,7 +3450,7 @@ const CastShadow = (p) => {
         points: `${x1},${y1} ${x2},${y2} ${x3},${y3} ${x4},${y4}`,
         fill: `url(#${gradId})`,
         pointerEvents: "none",
-        shapeRendering: "crispEdges"
+        shapeRendering: "optimizeSpeed"
       }
     )
   ] });
